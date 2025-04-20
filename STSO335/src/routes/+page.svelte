9eaa878ff2f14c66f39f817164b278ed9ecdddee
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2025-04-20 12:59:21 -0400

    added juxtapose highland park

--- STSO335/src/routes/+page.svelte
+++ STSO335/src/routes/+page.svelte
@@ -25,9 +25,25 @@
 	<p>To Olmsted, the park functioned as a remedy against contemporary urban problems by providing spaces for relaxation alongside nature. <a href="https://www.jstor.org/stable/23209954?searchText=frederick+law+olmsted&searchUri=%2Faction%2FdoBasicSearch%3FQuery%3Dfrederick%2Blaw%2Bolmsted%26so%3Drel&ab_segments=0%2Fbasic_search_gsv2%2Fcontrol&refreqid=fastly-default%3Ad03b114470cefc9416cab604d04d3892&seq=1">But this nature was not natural, instead the parks dreamt by Olmsted were highly manicured. His most famous work, Central Park, exists as a human-designed landscape instead of natural wilderness since its creators shaped it to create the illusion of organic beauty (Fisher, 2011, p. 2).</a> The artificial natural  environment played a fundamental role in achieving his design objectives. It presented both visual appeal and serenity alongside providing the community with a free to access forum to promote social ideals. The parks aimed to extract people out of immoral or socially repressive settings, such as saloons or tenement environments and lead them toward a collective civic experience. <a href="https://thesolutionsjournal.com/frederick-law-olmsted-nature-sustainable-communities/">The park served Olmsted as a democratic space according to Rome because it allowed people from different social classes to interact with each other.</a></p>
 
 	<p>Olmsted discovered in Rochester an audience which accepted his ideas although they showed restraint toward them. The city achieved the status of being one of only four places to receive an entire Olmsted park system. Highland Park together with Seneca Park and Genesee Valley Park followed his design approach by incorporating pastoral elements with accessible routes and open fields as well as water access points. The city of Rochester became the first metropolitan area of its class to develop such an extensive park planning initiative. Olmsted commended Rochester leaders for establishing public domains despite operating with limited financial resources.<a href="https://www.libraryweb.org/~rochhist/v75_2013/v75i2.pdf"> The city leaders who in  1888 hired the nation's premier expert in park design... decided to make public space their top priority  (Comeau, 2013, p. 1).</a> The city of Rochester provided more  than financial support by embracing a developing social movement dedicated to enhancing urban living conditions for city inhabitants.</p>
+
+	<iframe title="Highland Park" frameborder="0" class="juxtapose" width="100vw" height="auto" src="https://cdn.knightlab.com/libs/juxtapose/latest/embed/index.html?uid=0a121954-1e07-11f0-bb23-0936e1cb08fb"></iframe>
+
+
 </section>
 
 <style>
+iframe{
+	min-width: 100vw;
+	min-height:100vh;
+}
+
+	#juxtapose-embed{
+		min-width: 75vw;
+		min-height: 75vh;
+		padding-left: 12vw;
+		padding-right: 12vw;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
