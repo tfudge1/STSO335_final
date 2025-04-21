4ee601c4f85ffc074e416fc46035c07dd135b478
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2025-04-21 15:32:36 -0400

    created Central Park tangent page

---
+++ STSO335/src/routes/centralPark/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<title>Central Park</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<img src="https://content.time.com/time/2012/influentialplaces/centralpark.jpg" alt="Central Park from above"/>
+	<p class='footer-citation'>Taylor Swift, <a href="https://content.time.com/time/specials/packages/article/0,28804,2112644_2112566_2112482,00.html">Time Magazine,</a></p>
+	<p>Located in the center of Manhattan, Central Park is a landmark well known even far outside the city. Characterized both by sprawling fields as well as the stark juxtaposition between such pastoral fields and the intense urbanization of midtown. On either side of Central Park lies the Upper West Side (UWS) and the Upper East Side (UES), both of these neighborhoods are primarily residential with an estimated population of 433,964 people (Furmancenter). </p>
+	<p>It is impossible to imagine a Manhattan or a New York City without Central Park, but it is certain that the city would be unrecognizable. As anyone who has lived in NYC during the hot summer months will tell you, traveling from downtown to Central Park West or 5th Ave provides a breath of fresh air from the suffocating concrete oven that is the rest of the city. This oasis of green fields and blue ponds facilitates a often much needed escape, both mental and physical (temperature).</p>
+	<img src="https://images.foxtv.com/static.fox5ny.com/www.fox5ny.com/content/uploads/2021/03/932/470/AP_Central_Park_birdwatching_2.jpg?ve=1&tl=1" alt="Birdwatchers" />
+	<p class="footer-citation"><a href="https://www.fox5ny.com/news/bird-watching-is-booming-in-central-park-this-spring-migration">Fox 5</a>People try to get a look at and pictures of a Mandarin duck in Central Park in New York, Dec. 5, 2018. (AP Photo/Seth Wenig, File)</p>
+	<p>This escape from the brutality and speed of the ‘Big Apple’ is not only for Humans in the City, but wildlife as well. The giant green landingstrip of Central Park acts as a layover for migratory birds, this facilitates a large community of New York birdwatchers <a href="http://www.philjeffrey.net/cpb_bySeason.html">(Phil Jeffrey).</a> As well as living local legends of the peregrine falcons that have made home in the urban jungle <a href="https://www.urbanhawks.com/category/86th-and-wea-peregrine-falcons">(Urban Hawks and other wildlife in Central Park & NYC).</a></p>
+	<p>Central Park creates an environment that helps build communities beyond just birdwatchers. The more permanent Shakespeare in the park has been putting on free shows for the public over the past 60 years <a href="https://publictheater.org/about/delacorte-revitalization/?gad_source=1&gbraid=0AAAAADsLgKJguo9e1iqpBhEmopnmZCtkM&gclid=Cj0KCQjw2ZfABhDBARIsAHFTxGw_IYam0GU8IiYtqzwDWqdsEicIrZxQsaB-GjHm5KDc3XNDxFS-t9AaAnFqEALw_wcB">(Public theatre .org).</a> Allowing New Yorkers of all classes and walks of life to enjoy public theatre. The New York Philharmonic also hosts annual concerts in Central Park drawing massive crowds of a variety of New Yorkers.</p>
+</div>
+<style>
+	.footer-citation{
+		font-size: .7em;
+		color: #535454;
+	}
+	p{
+		text-indent: 10vw;
+		margin-bottom: 1.5em;
+	}
+</style>
